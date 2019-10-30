42ea6486c7c3a756836354de9ab8d5f332f59ad6
Piersanti dylan <dylngae@outlook.com>
2019-10-30 11:15:56 +0100

    Replace firt content

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,13 @@
 $(function() {
-  // Enjoy :)
+  $.ajax({
+    url: 'ajax/content.html',
+    type: 'GET',
+    success: function (html) {
+      console.log(html)
+      $('#ajaxFlower').html(html)
+    },
+    error: function (response) {
+      console.log('error')
+    }
+  })
 });
