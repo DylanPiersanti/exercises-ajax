12040091ff662157dad44f4bc093088d7cc1cda6
Piersanti dylan <dylngae@outlook.com>
2019-10-30 11:28:43 +0100

    setTimeout

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,23 @@ $(function() {
     url: 'ajax/content.html',
     type: 'GET',
     success: function (html) {
-      console.log(html)
       $('#ajaxFlower').html(html)
     },
     error: function (response) {
       console.log('error')
     }
   })
+
+  var sTimeOut = setTimeout(function () {
+    $.ajax({
+      url: 'ajax/delay5.html',
+      type: 'GET',
+      success: function (html) {
+        $('#timeOut').html(html)
+      },
+      error: function (response) {
+        console.log('error')
+      }
+    })
+  }, 5000)
 });
